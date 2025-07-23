4315fc3dbea6d5930769f6a66adeb2c17ee22df2
AlondraHackerBae <khrissy921@gmail.com>
2025-07-22 19:48:11 -0500

    partially added api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 function showAnswer(response) {
-  console.log(response);
+  alert(response.data.answer);
 }
 
+function revealGender(event) {
+  event.preventDefault();
+  console.log("finding names...");
+}
+
+let genderInput = document.querySelector("#gender");
 let apiKey = "8590002d4163ob0d0at3864bcd25fb7b";
-let prompt = "What are unique baby names for 2025";
-let context = " Make sure to provide a clear and precise answer";
+let prompt = `What are unique baby names for ${genderInput} gender`;
+let context =
+  " You are a connoisseur in naming babies and pull from a rolodex of names from countries all over the world";
 let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(url).then(showAnswer);
