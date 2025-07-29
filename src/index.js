2fdcea0df27bc29f779c6894d237aacbc222b5d8
AlondraHackerBae <khrissy921@gmail.com>
2025-07-29 12:45:24 -0500

    working on js functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-function showAnswer(response) {
+function revealBabyName(response) {
   alert(response.data.answer);
 }
 
-function revealGender(event) {
+function generateBabyName(event) {
   event.preventDefault();
-  console.log("finding names...");
+
+  let genderInput = document.querySelector("#gender");
+
+  let apiKey = "8590002d4163ob0d0at3864bcd25fb7b";
+  let prompt = `What are unique baby names for ${genderInput.value} gender`;
+  let context =
+    " You are a connoisseur in naming babies and pull from a rolodex of names from countries all over the world, only provide 4 options";
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(url).then(revealBabyName);
 }
 
-let genderInput = document.querySelector("#gender");
-let apiKey = "8590002d4163ob0d0at3864bcd25fb7b";
-let prompt = `What are unique baby names for ${genderInput} gender`;
-let context =
-  " You are a connoisseur in naming babies and pull from a rolodex of names from countries all over the world";
-let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-axios.get(url).then(showAnswer);
+let babyGender = document.querySelector("#gender");
+babyGender.addEventListener("click", generateBabyName);
